49b8f20d6eba6b01896dd1ec294fab2eb662c72f
akashpatel268 <akashpatel268@gmail.com>
2021-01-16 21:07:07 +0530

    Minimum range query segment tree implementation

---
+++ segment-tree/MinQuery.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void createMinSegmentTree(long int arr[], long int l, long int r, long int pos, long int min_seg[])
+{
+    if (l == r)
+    {
+        min_seg[pos] = arr[l];
+        return;
+    }
+    long int mid = (l + r) / 2;
+    createMinSegmentTree(arr, l, mid, 2 * pos + 1, min_seg);
+    createMinSegmentTree(arr, mid + 1, r, 2 * pos + 2, min_seg);
+    min_seg[pos] = min(min_seg[2 * pos + 1], min_seg[2 * pos + 2]);
+}
+
+long int query(long int l_index, long int r_index, long int pos, long int min_seg[], long int l, long int r)
+{
+    if (l_index > r || r_index < l)
+        return INT_MAX;
+    if (l_index >= l && r_index <= r)
+        return min_seg[pos];
+    int mid = (l_index + r_index) / 2;
+    long int left_min = query(l_index, mid, 2 * pos + 1, min_seg, l, r);
+    long int right_min = query(mid + 1, r_index, 2 * pos + 2, min_seg, l, r);
+    return min(left_min, right_min);
+}
+
+int main()
+{
+    long int n, q, l, r;
+    cin >> n;
+    long int arr[n], min_seg[4 * n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    createMinSegmentTree(arr, 0, n - 1, 0, min_seg);
+    cin >> q;
+    while (q--)
+    {
+        cin >> l >> r;
+        cout << query(0, n - 1, 0, min_seg, l, r) << endl;
+    }
+}
